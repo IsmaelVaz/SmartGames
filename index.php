9d0374a4b92c1267b76508277f1530548718eabb
IsmaelVaz <ismaelvaz18@hotmail.com>
2016-12-15 22:50:19 -0200

    Layout da tela de login

---
+++ index.php
@@ -0,0 +1,16 @@
+<html lang="pt-br">
+    <head>
+        <title>Smart Games</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+    </head>
+    <body>
+        <div id="login">
+            <form action="index.php" method="get" name="login">
+                <input type="email" placeholder="E-mail de acesso" name="txt_email"/>
+                <input type="password" placeholder="Senha de acesso" name="txt_senha"/>
+                <input type="submit" name="btn_entrar" value="Entrar"/>
+            </form>
+        </div>
+    </body>
+</html>
